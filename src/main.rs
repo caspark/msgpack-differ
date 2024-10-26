c3efeb235273dd2bad961dc1a78715db041c0df0
Caspar Krieger <git@asparck.com>
2024-10-26 21:37:05 +0800

    Add title

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use eframe::egui;
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default(),
+        viewport: egui::ViewportBuilder::default().with_title("MsgPack Differ"),
         ..Default::default()
     };
     eframe::run_native(
